e8956fa218082d78b8c9f83935428c1e50c33677
lanyi <lanyilee@qq.com>
2018-12-20 18:11:15 +0800

    micro server

---
+++ src/user-server/database.go
@@ -0,0 +1,17 @@
+package user_server
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func (config Config) CreateConnection() *sql.DB {
+	DB, err := sql.Open(config.DBType, config.DBHost)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//don't forget to close the db
+	// defer DB.Close()
+	return DB
+}
---
+++ src/user-server/utils.go
@@ -0,0 +1,28 @@
+package user_server
+
+import (
+	"fmt"
+	"gopkg.in/ini.v1"
+)
+
+type Config struct {
+	DBType string `int:"DBType"`
+	DBHost string `int:"DBHost"`
+}
+
+//读取配置文件并转成结构体
+func ReadConfig(path string) (Config, error) {
+	var config Config
+	conf, err := ini.Load(path) //加载配置文件
+	if err != nil {
+		fmt.Println("load config file fail!")
+		return config, err
+	}
+	conf.BlockMode = false
+	err = conf.MapTo(&config) //解析成结构体
+	if err != nil {
+		fmt.Println("mapto config file fail!")
+		return config, err
+	}
+	return config, nil
+}
