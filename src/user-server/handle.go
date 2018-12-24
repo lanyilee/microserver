aa700f137611363f8f6c99e70623a1bfadf1326c
lanyi <lanyilee@qq.com>
2018-12-24 18:15:07 +0800

    micro handler

--- src/user-server/handle.go
+++ src/user-server/handle.go
@@ -2,19 +2,68 @@ package user_server
 
 import (
 	"../github.com/micro/go-micro"
-	"context"
 	pb "./proto/user"
-	"golang.org/x/crypto/go get -u golang.org/x/crypto/..."
+	"context"
+	"github.com/micro/go-micro/errors"
+	_ "github.com/micro/go-plugins/broker/nats"
+	"golang.org/x/crypto/bcrypt"
+	"log"
 )
+
 const topic = "user.created"
 
 type handler struct {
-	Repo Repository
+	Repo         Repository
 	TokenService Authable
-	Publisher micro.Publisher
+	Publisher    micro.Publisher
 }
 
-func (h *handler) Create(ctx context.Context,req *pb.User,resp *pb.Response)error{
+func (h *handler) Create(ctx context.Context, req *pb.User, resp *pb.Response) error {
 	//hash password
+	hashedPwd, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Panic(err)
+		return err
+	}
+	req.Password = string(hashedPwd)
+	err = h.Repo.Create(req)
+	if err != nil {
+		log.Panic(err)
+		return err
+	}
+	resp.User = req
+	// publish user
+	if err := h.Publisher.Publish(ctx, req); err != nil {
+		return err
+	}
+	return nil
+}
 
+func (h *handler) Auth(ctx context.Context, req *pb.User, resp *pb.Token) error {
+	u, err := h.Repo.GetByEmail(req.Email)
+	if err != nil {
+		log.Panic(err)
+		return err
+	}
+	if err = bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(req.Password)); err != nil {
+		log.Panic(err)
+		return err
+	}
+	//to generate token
+	t, err := h.TokenService.Encode(u)
+	resp.Token = t
+	return nil
+}
+
+func (h *handler) ValidateToken(ctx context.Context, req *pb.Token, resp *pb.Token) error {
+	claim, err := h.TokenService.Decode(req.Token)
+	if err != nil {
+		log.Panic(err)
+		return err
+	}
+	if claim.User.Id == "" {
+		return errors.New("1", "invalid user", 101)
+	}
+	resp.Valid = true
+	return nil
 }
--- src/user-server/repository.go
+++ src/user-server/repository.go
@@ -7,50 +7,50 @@ import (
 )
 
 type Repository interface {
-	Get(id string) (*pb.User,error)
-	GetAll()([]*pb.User,error)
-	Create(*pb.User,error)
-	GetByEmail(email string)(*pb.User,error)
+	Get(id string) (*pb.User, error)
+	GetAll() ([]*pb.User, error)
+	Create(*pb.User) error
+	GetByEmail(email string) (*pb.User, error)
 }
 
 type UserRepository struct {
 	db *sql.DB
 }
 
-func (ur *UserRepository) Get(id string) (*pb.User,error){
-	user:=&pb.User{}
-	db:=ur.db
-	err:=db.QueryRow("select * from t_user where  id = ? ",id).Scan(&user.Id,&user.Name,&user.Password,&user.Company,&user.Email)
+func (ur *UserRepository) Get(id string) (*pb.User, error) {
+	user := &pb.User{}
+	db := ur.db
+	err := db.QueryRow("select * from t_user where  id = ? ", id).Scan(&user.Id, &user.Name, &user.Password, &user.Company, &user.Email)
 	if err != nil {
 		log.Fatalln(err)
-		return nil,err
+		return nil, err
 	}
-	return user,nil
+	return user, nil
 }
 
-func (ur *UserRepository)GetAll()([]*pb.User,error){
-	db:=ur.db
-	users:=[]*pb.User{}
-	rows,err:=db.Query("select * from t_user ")
-	for rows.Next(){
-		user:=pb.User{}
-		err:=rows.Scan(&user.Id,&user.Name,&user.Password,&user.Company,&user.Email)
+func (ur *UserRepository) GetAll() ([]*pb.User, error) {
+	db := ur.db
+	users := []*pb.User{}
+	rows, err := db.Query("select * from t_user ")
+	for rows.Next() {
+		user := pb.User{}
+		err := rows.Scan(&user.Id, &user.Name, &user.Password, &user.Company, &user.Email)
 		if err != nil {
 			log.Fatalln(err)
-			return nil,err
+			return nil, err
 		}
-		users=append(users,&user)
+		users = append(users, &user)
 	}
 	if err != nil {
 		log.Fatalln(err)
-		return nil,err
+		return nil, err
 	}
-	return users,nil
+	return users, nil
 }
 
-func (ur *UserRepository)Create(user *pb.User)error{
-	db:=ur.db
-	_,err:=db.Exec("insert into t_user values(?,?,?,?,?)",user.Id,user.Name,user.Password,user.Company,user.Email)
+func (ur *UserRepository) Create(user *pb.User) error {
+	db := ur.db
+	_, err := db.Exec("insert into t_user values(?,?,?,?,?)", user.Id, user.Name, user.Password, user.Company, user.Email)
 	if err != nil {
 		log.Fatalln(err)
 		return err
@@ -58,13 +58,13 @@ func (ur *UserRepository)Create(user *pb.User)error{
 	return nil
 }
 
-func (ur *UserRepository)GetByEmail(email string)(*pb.User,error){
-	user:=&pb.User{}
-	db:=ur.db
-	err:=db.QueryRow("select * from t_user where email = ?",email).Scan(&user.Id,&user.Name,&user.Password,&user.Company,&user.Email)
-	if err!=nil{
+func (ur *UserRepository) GetByEmail(email string) (*pb.User, error) {
+	user := &pb.User{}
+	db := ur.db
+	err := db.QueryRow("select * from t_user where email = ?", email).Scan(&user.Id, &user.Name, &user.Password, &user.Company, &user.Email)
+	if err != nil {
 		log.Fatalln(err)
-		return nil,err
+		return nil, err
 	}
-	return user,nil
+	return user, nil
 }
